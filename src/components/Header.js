3728ecd4da786730ab063879a2c9bb8414ce1cb1
dragosbus <dragosbusuioc94@gmail.com>
2018-06-26 15:45:20 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const Header = () =>{
+    return(
+        <header className="main-header">
+            <h1 className="logo">Pets</h1>
+        </header>
+    );
+};
